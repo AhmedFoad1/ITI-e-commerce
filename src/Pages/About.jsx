c9e44d074f8a1a28f6d312a47c0876da45034487
ahmed <ahmedmohamedfoad109@gmail.com>
2024-12-25 23:49:10 +0200

    Update About.jsx

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,27 +1,26 @@
-
-
 function About() {
   return (
-    <div className=""><div className="sm:flex  items-center min-h-[90vh] min-w-screen-xl">
-    <div className="sm:w-1/2  flex  justify-center  my-auto">
-        <div className="image object-center text-center">
-            <img src="https://i.imgur.com/WbQnbas.png"/>
+    <div className="min-h-[90vh] min-w-screen-xl sm:flex items-center">
+      <div className="sm:w-1/2 flex justify-center my-auto">
+        <div className="text-center">
+          <img src="https://i.imgur.com/WbQnbas.png" alt="Company"/>
         </div>
-    </div>
-    <div className="sm:w-1/2 p-5 ">
+      </div>
+      <div className="sm:w-1/2 p-5">
         <div className="text">
-            <span className="text-gray-500 border-b-2 border-indigo-600 uppercase">About us</span>
-            <h2 className="my-4 font-bold text-3xl  sm:text-4xl ">About <span className="text-indigo-600">Our Company</span>
-            </h2>
-            <p className="text-gray-700">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, commodi
-                doloremque, fugiat illum magni minus nisi nulla numquam obcaecati placeat quia, repellat tempore
-                voluptatum.
-            </p>
+          <span className="text-gray-500 border-b-2 border-indigo-600 uppercase">About us</span>
+          <h2 className="my-4 font-bold text-3xl sm:text-4xl">
+            About <span className="text-indigo-600">Our Company</span>
+          </h2>
+          <p className="text-gray-700">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, commodi
+            doloremque, fugiat illum magni minus nisi nulla numquam obcaecati placeat quia, repellat tempore
+            voluptatum.
+          </p>
         </div>
+      </div>
     </div>
-</div> </div>
-  )
+  );
 }
 
-export default About
+export default About;
